9f391ad5bb1cef9986fd213f44684f4ba3a6a096
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-07-26 13:19:38 -0300

    adicionando validação

--- src/main/java/br/com/alura/forum/dto/form/TopicoForm.java
+++ src/main/java/br/com/alura/forum/dto/form/TopicoForm.java
@@ -4,9 +4,25 @@ import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 public class TopicoForm {
+
+	@NotNull
+	@NotEmpty
+	@Length(min = 5)
 	private String titulo;
+
+	@NotNull
+	@NotEmpty
+	@Length(min = 10)
 	private String mensagem;
+
+	@NotNull
+	@NotEmpty
 	private String nomeCurso;
 
 	public String getTitulo() {
