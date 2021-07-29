ac80be36313bfe2db74680dfd488c759be996d2c
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-07-29 09:53:43 -0300

    implementando o spring security

---
+++ src/main/java/br/com/alura/forum/config/secutiry/SecurityConfigurations.java
@@ -0,0 +1,11 @@
+package br.com.alura.forum.config.secutiry;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+}
