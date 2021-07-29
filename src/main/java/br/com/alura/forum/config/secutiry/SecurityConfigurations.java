265102a5c412b3d0cede7084e8bb1b5b7e702624
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-07-29 10:32:23 -0300

    configurando autorização

--- src/main/java/br/com/alura/forum/config/secutiry/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/secutiry/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package br.com.alura.forum.config.secutiry;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +12,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+	// Configuração de autenticação
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	}
+
+	// Configuração de autorização
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()//
+				.antMatchers(HttpMethod.GET, "/topicos").permitAll()//
+				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()//
+				.anyRequest().authenticated();
+	}
+
+	// Configuração de recusos (js, css, imagens etc.)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	}
 }
