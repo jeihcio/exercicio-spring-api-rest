fc021a5308ef2020ad57a2f34c970481988adb94
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-08-01 20:52:39 -0300

    configurando parâmetro global para o swagger

--- src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
@@ -1,11 +1,16 @@
 package br.com.alura.forum.config.swagger;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.alura.forum.modelo.Usuario;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -13,12 +18,23 @@ import springfox.documentation.spring.web.plugins.Docket;
 public class SwaggerConfigurations {
 
 	@Bean
-	public Docket forumApi() {
+	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)//
 				.select()//
 				.apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))//
 				.paths(PathSelectors.ant("/**"))//
 				.build()//
-				.ignoredParameterTypes(Usuario.class);
+				.ignoredParameterTypes(Usuario.class)//
+				.globalOperationParameters(//
+						Arrays.asList(//
+								new ParameterBuilder()//
+										.name("Authorization")//
+										.description("Header para Token JWT")//
+										.modelRef(new ModelRef("string"))//
+										.parameterType("header")//
+										.required(false)//
+										.build()//
+						));
 	}
+
 }
