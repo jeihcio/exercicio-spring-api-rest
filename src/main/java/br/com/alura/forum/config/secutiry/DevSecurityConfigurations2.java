bfec7096fe89a28f5bd9cf18968a2787ded99f6b
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-08-02 17:14:26 -0300

    adicionando profile

---
+++ src/main/java/br/com/alura/forum/config/secutiry/DevSecurityConfigurations2.java
@@ -0,0 +1,22 @@
+package br.com.alura.forum.config.secutiry;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@Profile("dev")
+public class DevSecurityConfigurations2 extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()//
+				.antMatchers("/**").permitAll()//
+				.anyRequest().authenticated()//
+				.and().csrf().disable();
+	}
+}
