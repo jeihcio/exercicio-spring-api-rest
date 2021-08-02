31155ab5233ad5120556de7fd38c9b14cd8a5158
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-08-02 17:20:14 -0300

    removendo imports

--- src/main/java/br/com/alura/forum/config/secutiry/DevSecurityConfigurations2.java
+++ src/main/java/br/com/alura/forum/config/secutiry/DevSecurityConfigurations2.java
@@ -2,7 +2,6 @@ package br.com.alura.forum.config.secutiry;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
