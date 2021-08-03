cb8e13fcfb4c86bb872d4f344b0796852051c202
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-08-03 16:35:14 -0300

    Criando uma classe de test

---
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -0,0 +1,34 @@
+package br.com.alura.forum.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import br.com.alura.forum.modelo.Curso;
+
+@DataJpaTest
+public class CursoRepositoryTest {
+	
+	@Autowired
+	private CursoRepository repository;
+
+	@Test
+	public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
+		String nomeCurso = "HTML 5";
+		Curso curso = repository.findByNome(nomeCurso);
+		
+		assertNotNull(curso);
+		assertEquals(nomeCurso, curso.getNome());
+	}
+	
+	@Test
+	public void naoDeveriaCarregarUmCursoCujoNomeNaoEstejaCadastrado() {
+		String nomeCurso = "JPA";
+		Curso curso = repository.findByNome(nomeCurso);
+		
+		assertNull(curso);
+	}
+
+}
